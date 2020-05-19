818bcd74b1e795f8560e93050b25b131532d8098
Okpara Obinna <obainoforgod@gmail.com>
2020-05-19 13:42:15 +0100

    reverse

--- obivideochat/Startup.cs
+++ obivideochat/Startup.cs
@@ -55,14 +55,14 @@ namespace obivideochat
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
-            var options = new RewriteOptions()
-                .AddRedirectToHttpsPermanent();
+            //var options = new RewriteOptions()
+            //    .AddRedirectToHttpsPermanent();
 
-            app.UseRewriter(options);
+            //app.UseRewriter(options);
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCors("CorsPolicy");
 
